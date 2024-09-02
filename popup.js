2ee236aafa94ce2e6efd89440a3ba03a7c07ee33
Manasseh <believemanasseh@gmail.com>
2024-09-02 12:16:22 +0100

    feat: modify popup script

--- popup.js
+++ popup.js
@@ -4,11 +4,13 @@ document.getElementById("btn").addEventListener("click", () => {
     const button = document.getElementById("btn");
 
     if (isPurifying) {
-      chrome.tabs.sendMessage(tab.id, { action: "stopPurifying" });
       button.textContent = "START PURIFICATION";
+      button.style.backgroundColor = "rgb(57, 115, 202)";
+      chrome.tabs.sendMessage(tab[0].id, { action: "stopPurifying" });
     } else {
-      chrome.tabs.sendMessage(tab.id, { action: "startPurifying" });
       button.textContent = "STOP PURIFICATION";
+      button.style.backgroundColor = "red";
+      chrome.tabs.sendMessage(tab[0].id, { action: "startPurifying" });
     }
 
     localStorage.setItem("isPurifying", !isPurifying);
