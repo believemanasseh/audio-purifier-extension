257c38459cff679518cb6b93195d4dda8d06b051
Manasseh <believemanasseh@gmail.com>
2024-09-25 23:43:27 +0100

    feat: modify popup and content scripts

--- content.js
+++ content.js
@@ -1,82 +1,18 @@
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  let socket;
-  let analyser;
-  let dataArray;
-
-  if (message.action === "startPurifying") {
-    const audioContext = new AudioContext();
-    audioContext.createAnalyser();
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: false,
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "popup" && message.action === "startPurifying") {
+    chrome.runtime.sendMessage({
+      type: "content",
+      action: "startPurifying",
     });
-
-    // Create input, effect and analyser audio nodes
-    const source = audioContext.createMediaStreamSource(stream);
-    const filter = audioContext.createBiquadFilter();
-    analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048;
-    const bufferLength = analyser.frequencyBinCount;
-    dataArray = new Uint8Array(bufferLength);
-
-    visualise();
-
-    // Configure Biquad filter
-    filter.type = "lowpass";
-    filter.frequency.setValueAtTime(1000, audioContext.currentTime);
-    filter.gain.setValueAtTime(25, audioContext.currentTime);
-
-    // Connect nodes
-    source.connect(filter);
-    filter.connect(audioContext.destination);
-    analyser.connect(audioContext.destination);
-
-    socket = new WebSocket("ws://cloud-api-server");
-
-    let outputData;
-
-    socket.onopen = () => {
-      filter.onaudioprocess = (audioProcessingEvent) => {
-        const inputData = audioProcessingEvent.inputBuffer.getChannelData(0);
-        outputData = audioProcessingEvent.outputBuffer.getChannelData(0);
-        const audioInput = Array.from(inputData);
-        socket.send({ audioInput });
-      };
-    };
-
-    socket.onmessage = (event) => {
-      outputData[0] = event.data.audioOutput;
-    };
-
-    socket.onerror = (error) => {
-      console.error("WebSocket error:", error);
-    };
-
-    socket.onclose = () => {
-      console.log("WebSocket connection closed");
-    };
-  } else if (message.action === "stopPurifying") {
-    socket.close();
+    sendResponse({ status: "Purification started" });
+    localStorage.setItem("isPurifying", true);
+  } else if (message.type === "popup" && message.action === "stopPurifying") {
+    chrome.runtime.sendMessage({
+      type: "content",
+      action: "stopPurifying",
+    });
+    sendResponse({ status: "Purification stopped" });
+    localStorage.setItem("isPurifying", false);
   }
-
-  const visualise = () => {
-    analyser.getByteFrequencyData(dataArray);
-
-    const canvas = document.getElementById("spectrogram");
-    const canvasCtx = canvas.getContext("2d");
-    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw spectrogram
-    for (let i = 0; i < dataArray.length; i++) {
-      const barHeight = (dataArray[i] / 255) * canvas.height;
-      const barWidth = canvas.width / dataArray.length;
-      const x = i * barWidth;
-      const y = canvas.height - barHeight;
-
-      canvasCtx.fillStyle = `rgb(${dataArray[i]}, 0, 0)`;
-      canvasCtx.fillRect(x, y, barWidth, barHeight);
-    }
-
-    requestAnimationFrame(visualise);
-  };
+  return true;
 });
--- popup.js
+++ popup.js
@@ -1,18 +1,50 @@
-document.getElementById("btn").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tab) => {
+const START_TEXT = "START PURIFICATION";
+const STOP_TEXT = "STOP PURIFICATION";
+const START_BTN_COLOR = "rgb(57, 115, 202)";
+const STOP_BTN_COLOR = "red";
+
+const btn = document.getElementById("btn");
+
+let isPurifying = localStorage.getItem("isPurifying") === "true";
+
+if (isPurifying) {
+  btn.textContent = STOP_TEXT;
+  btn.style.backgroundColor = STOP_BTN_COLOR;
+} else {
+  btn.textContent = START_TEXT;
+  btn.style.backgroundColor = START_BTN_COLOR;
+}
+
+btn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    const tabId = tabs[0].id;
     const isPurifying = localStorage.getItem("isPurifying") === "true";
-    const button = document.getElementById("btn");
+    const action = isPurifying ? "stopPurifying" : "startPurifying";
 
-    if (isPurifying) {
-      button.textContent = "START PURIFICATION";
-      button.style.backgroundColor = "rgb(57, 115, 202)";
-      chrome.tabs.sendMessage(tab[0].id, { action: "stopPurifying" });
-    } else {
-      button.textContent = "STOP PURIFICATION";
-      button.style.backgroundColor = "red";
-      chrome.tabs.sendMessage(tab[0].id, { action: "startPurifying" });
-    }
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabId },
+        files: ["content.js"],
+      },
+      () => {
+        localStorage.setItem("tabId", tabId);
 
-    localStorage.setItem("isPurifying", !isPurifying);
+        if (isPurifying) {
+          btn.textContent = START_TEXT;
+          btn.style.backgroundColor = START_BTN_COLOR;
+          chrome.tabs.sendMessage(tabId, {
+            type: "popup",
+            action: action,
+          });
+        } else {
+          btn.textContent = STOP_TEXT;
+          btn.style.backgroundColor = STOP_BTN_COLOR;
+          chrome.tabs.sendMessage(tabId, {
+            type: "popup",
+            action: action,
+          });
+        }
+      }
+    );
   });
 });
