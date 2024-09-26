1df41e5c4229f3fa84816d439d2b333bfbda0bc3
Manasseh <believemanasseh@gmail.com>
2024-09-26 01:52:06 +0100

    feat: use content script for purification process and worker script for long-running websocket connection

--- content.js
+++ content.js
@@ -1,18 +1,76 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+let audioContext;
+let stream;
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.type === "popup" && message.action === "startPurifying") {
-    chrome.runtime.sendMessage({
-      type: "content",
-      action: "startPurifying",
-    });
+    chrome.runtime.sendMessage({ action: "startWebSocketConnection" });
+    await startPurification();
     sendResponse({ status: "Purification started" });
-    localStorage.setItem("isPurifying", true);
-  } else if (message.type === "popup" && message.action === "stopPurifying") {
-    chrome.runtime.sendMessage({
-      type: "content",
-      action: "stopPurifying",
-    });
-    sendResponse({ status: "Purification stopped" });
-    localStorage.setItem("isPurifying", false);
   }
+
+  if (message.type === "popup" && message.action === "stopPurifying") {
+    chrome.runtime.sendMessage({ action: "stopWebSocketConnection" });
+    stopPurification();
+    sendResponse({ status: "Purification stopped" });
+  }
+
+  if (message.action === "playAudio") {
+    playProcessedAudio(message.audioData);
+  }
+
   return true;
 });
+
+async function startPurification() {
+  audioContext = new AudioContext();
+
+  await audioContext.audioWorklet.addModule("processor.js");
+
+  // Get audio stream from the microphone
+  stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  const source = audioContext.createMediaStreamSource(stream);
+  const filter = audioContext.createBiquadFilter();
+
+  filter.type = "lowpass";
+  filter.frequency.setValueAtTime(1000, audioContext.currentTime);
+  filter.gain.setValueAtTime(25, audioContext.currentTime);
+
+  const workletNode = new AudioWorkletNode(audioContext, "audio-processor");
+
+  source.connect(filter);
+  filter.connect(workletNode);
+  workletNode.connect(audioContext.destination);
+
+  localStorage.setItem("isPurifying", true);
+
+  // Handle messages from the AudioWorkletNode
+  workletNode.port.onmessage = (event) => {
+    const processedAudio = event.data;
+
+    chrome.runtime.sendMessage({
+      action: "sendAudioData",
+      audioData: processedAudio,
+    });
+  };
+}
+
+function stopPurification() {
+  if (audioContext) {
+    audioContext.close();
+  }
+
+  if (stream) {
+    stream.getTracks().forEach((track) => track.stop());
+  }
+
+  localStorage.setItem("isPurifying", false);
+
+  console.log("Purification stopped");
+}
+
+function playProcessedAudio(data) {
+  const blob = new Blob([data], { type: "audio/wav" });
+  const url = URL.createObjectURL(blob);
+  const audio = new Audio(url);
+  audio.play();
+}
--- worker.js
+++ worker.js
@@ -1,20 +1,22 @@
 let socket = null;
 let isPurifying = false;
-let audioContext;
-let workletNode;
-let stream;
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Background script installed successfully!");
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "content" && message.action === "startPurifying") {
-    if (!isPurifying) {
-      startPurification();
-      isPurifying = true;
-      sendResponse({ status: "Purification started" });
+  if (message.action === "startWebSocketConnection" && !isPurifying) {
+    startWebSocketConnection(sendResponse);
+    isPurifying = true;
+    sendResponse({ status: "Purification started" });
+  }
+
+  if (message.action === "stopWebSocketConnection" && isPurifying) {
+    if (socket) {
+      socket.close();
     }
+    sendResponse({ status: "WebSocket stopped" });
   }
 
   if (message.type === "content" && message.action === "stopPurifying") {
@@ -24,9 +26,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       sendResponse({ status: "Purification stopped" });
     }
   }
+
+  if (
+    message.action === "sendAudioData" &&
+    socket &&
+    socket.readyState === WebSocket.OPEN
+  ) {
+    socket.send(message.audioData);
+  }
 });
 
-function startWebSocketConnection() {
+function startWebSocketConnection(sendResponse) {
   socket = new WebSocket("ws://cloud-api-server");
 
   socket.onopen = () => {
@@ -36,7 +46,7 @@ function startWebSocketConnection() {
   socket.onmessage = (event) => {
     const processedAudio = event.data;
     console.log("Received denoised audio data");
-    playProcessedAudio(processedAudio);
+    sendResponse({ action: "playAudio", audioData: processedAudio });
   };
 
   socket.onerror = (error) => {
@@ -46,56 +56,4 @@ function startWebSocketConnection() {
   socket.onclose = () => {
     console.log("WebSocket connection closed");
   };
-}
-
-async function startPurification() {
-  audioContext = new AudioContext();
-
-  startWebSocketConnection();
-
-  await audioContext.audioWorklet.addModule("processor.js");
-
-  // Get audio stream from the microphone
-  stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  const source = audioContext.createMediaStreamSource(stream);
-  const filter = audioContext.createBiquadFilter();
-
-  filter.type = "lowpass";
-  filter.frequency.setValueAtTime(1000, audioContext.currentTime);
-  filter.gain.setValueAtTime(25, audioContext.currentTime);
-
-  workletNode = new AudioWorkletNode(audioContext, "audio-processor");
-
-  source.connect(filter);
-  filter.connect(workletNode);
-  workletNode.connect(audioContext.destination);
-
-  // Handle messages from the AudioWorkletNode
-  workletNode.port.onmessage = (event) => {
-    const processedAudio = event.data;
-
-    if (socket && socket.readyState === WebSocket.OPEN) {
-      socket.send(processedAudio);
-    }
-  };
-}
-
-function stopPurification() {
-  if (audioContext) {
-    audioContext.close();
-    audioContext = null;
-  }
-
-  if (stream) {
-    stream.getTracks().forEach((track) => track.stop());
-  }
-
-  console.log("Purification stopped");
-}
-
-function playProcessedAudio(data) {
-  const blob = new Blob([data], { type: "audio/wav" });
-  const url = URL.createObjectURL(blob);
-  const audio = new Audio(url);
-  audio.play();
 }
