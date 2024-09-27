2d1a066ed92023a137afe723774643a5e1441bc3
Manasseh <believemanasseh@gmail.com>
2024-09-27 22:00:35 +0100

    feat: modify popup script to monitor purification state

--- popup.js
+++ popup.js
@@ -2,18 +2,9 @@ const START_TEXT = "START PURIFICATION";
 const STOP_TEXT = "STOP PURIFICATION";
 const START_BTN_COLOR = "rgb(57, 115, 202)";
 const STOP_BTN_COLOR = "red";
+let btn;
 
-const btn = document.getElementById("btn");
-
-let isPurifying = localStorage.getItem("isPurifying") === "true";
-
-if (isPurifying) {
-  btn.textContent = STOP_TEXT;
-  btn.style.backgroundColor = STOP_BTN_COLOR;
-} else {
-  btn.textContent = START_TEXT;
-  btn.style.backgroundColor = START_BTN_COLOR;
-}
+checkPurificationStatus();
 
 btn.addEventListener("click", () => {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
@@ -36,9 +27,20 @@ btn.addEventListener("click", () => {
           btn.style.backgroundColor = STOP_BTN_COLOR;
           chrome.tabs.sendMessage(tabId, { action: action });
         }
-
-        localStorage.setItem("tabId", tabId);
+        localStorage.setItem("isPurifying", !isPurifying);
       }
     );
   });
 });
+
+function checkPurificationStatus() {
+  btn = document.getElementById("btn");
+  const isPurifying = localStorage.getItem("isPurifying") === "true";
+  if (isPurifying) {
+    btn.textContent = STOP_TEXT;
+    btn.style.backgroundColor = STOP_BTN_COLOR;
+  } else {
+    btn.textContent = START_TEXT;
+    btn.style.backgroundColor = START_BTN_COLOR;
+  }
+}
