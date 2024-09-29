23b8e3ac40583453d4fa81c9255b3bbaf11ba532
Manasseh <believemanasseh@gmail.com>
2024-09-29 01:18:47 +0100

    feat: add frequency spectrogram visualiser function

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 if (typeof audioContext === "undefined") {
   var audioContext;
-}
-
-if (typeof stream === "undefined") {
   var stream;
+  var analyser;
+  var dataArray;
+  var bufferLength;
 }
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
@@ -35,12 +35,18 @@ async function startPurification() {
   const moduleURL = chrome.runtime.getURL("processor.js");
   await audioContext.audioWorklet.addModule(moduleURL);
 
+  // Create input, effect and analyser audio nodes
   const source = audioContext.createMediaStreamSource(stream);
-  const filter = audioContext.createBiquadFilter();
 
+  const filter = audioContext.createBiquadFilter();
   filter.type = "lowpass";
   filter.frequency.setValueAtTime(1000, audioContext.currentTime);
   filter.gain.setValueAtTime(25, audioContext.currentTime);
+
+  analyser = audioContext.createAnalyser();
+  analyser.fftSize = 256;
+  bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
 
   const workletNode = new AudioWorkletNode(audioContext, "audio-processor");
 
@@ -51,6 +57,9 @@ async function startPurification() {
   // Handle messages from the AudioWorkletNode
   workletNode.port.onmessage = (event) => {
     const processedAudio = event.data;
+
+    visualise(); // visualise spectrogram
+
     chrome.runtime.sendMessage({
       action: "sendAudioData",
       audioData: processedAudio,
@@ -73,4 +82,31 @@ function playProcessedAudio(data) {
   const url = URL.createObjectURL(blob);
   const audio = new Audio(url);
   audio.play();
+}
+
+function visualise() {
+  requestAnimationFrame(visualise);
+
+  analyser.getByteFrequencyData(dataArray);
+
+  const canvas = document.getElementById("spectrogram");
+  const canvasCtx = canvas.getContext("2d");
+  const HEIGHT = canvas.height;
+  const WIDTH = canvas.width;
+
+  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+  canvasCtx.fillStyle = "rgb(0, 0, 0)";
+  canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  const barWidth = (WIDTH / bufferLength) * 2.5;
+  let x = 0;
+
+  for (let i = 0; i < bufferLength; i++) {
+    const barHeight = dataArray[i];
+
+    canvasCtx.fillStyle = "rgb(" + (barHeight + 100) + ",50,50)";
+    canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+
+    x += barWidth + 1;
+  }
 }
