d370af0ff4507f7b038d4dbec4d2f52b03559c5c
Manasseh <believemanasseh@gmail.com>
2024-12-16 21:19:49 +0100

    feat(extension): add websocket reconnection function

--- extension/worker.js
+++ extension/worker.js
@@ -3,6 +3,8 @@ let isPurifying = false;
 let popupPort = null;
 let contentPort = null;
 let visualisationData = { dataArray: [], bufferLength: 0 };
+const WS_URL = "ws://localhost:8001/ws";
+const RECONNECT_DELAY = 3000;
 
 chrome.runtime.onConnect.addListener((port) => {
   if (port.name === "popup") {
@@ -30,64 +32,80 @@ chrome.runtime.onConnect.addListener((port) => {
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (!isPurifying && message.action === "startWebSocketConnection") {
-    startWebSocketConnection();
-    isPurifying = true;
-  }
+  switch (message.action) {
+    case "startWebSocketConnection":
+      if (!isPurifying) {
+        startWebSocketConnection();
+        isPurifying = true;
+      }
+      break;
+    case "stopWebSocketConnection":
+      if (isPurifying && socket) {
+        socket.close();
+        isPurifying = false;
+      }
+      break;
+    case "sendAudioData":
+      if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify({ noisy_audio: message.audioData }));
+      }
+    case "visualise":
+      visualisationData = {
+        dataArray: message.dataArray,
+        bufferLength: message.bufferLength,
+      };
 
-  if (isPurifying && socket && message.action === "stopWebSocketConnection") {
-    socket.close();
-    isPurifying = false;
-  }
-
-  if (
-    message.action === "sendAudioData" &&
-    socket &&
-    socket.readyState === WebSocket.OPEN
-  ) {
-    socket.send(JSON.stringify({ noisy_audio: message.audioData }));
-  }
-
-  if (message.action === "visualise") {
-    visualisationData = {
-      dataArray: message.dataArray,
-      bufferLength: message.bufferLength,
-    };
-
-    if (popupPort) {
-      popupPort.postMessage({
-        action: "visualise",
-        dataArray: visualisationData.dataArray,
-        bufferLength: visualisationData.bufferLength,
-      });
-    }
+      if (popupPort) {
+        popupPort.postMessage({
+          action: "visualise",
+          dataArray: visualisationData.dataArray,
+          bufferLength: visualisationData.bufferLength,
+        });
+      }
+      break;
+    default:
+      console.log("Invalid message");
   }
 });
 
 function startWebSocketConnection() {
-  socket = new WebSocket("ws://localhost:8000/ws");
+  socket = new WebSocket(WS_URL);
 
   socket.onopen = () => {
     console.log("WebSocket connection opened");
   };
 
   socket.onmessage = (event) => {
-    const processedAudio = event.data;
-    const denoisedAudio = JSON.parse(processedAudio).denoisedAudio;
+    try {
+      const processedAudio = event.data;
+      const denoisedAudio = JSON.parse(processedAudio).denoisedAudio;
 
-    if (contentPort) {
-      contentPort.postMessage({
-        action: "playAudio",
-        audioData: denoisedAudio,
-      });
+      if (contentPort) {
+        contentPort.postMessage({
+          action: "playAudio",
+          audioData: denoisedAudio,
+        });
+      }
+    } catch (err) {
+      console.error("Error processing websocket message: ", err);
     }
   };
 
   socket.onerror = (error) => {
     console.error("WebSocket Error: ", error);
+    reconnectWebSocket();
   };
 
   socket.onclose = () => {
     console.log("WebSocket connection closed");
   };
+}
+
+function reconnectWebSocket() {
+  if (isPurifying) {
+    setTimeout(() => {
+      console.log("Attempting to reconnect WebSocket...");
+      startWebSocketConnection();
+    }, RECONNECT_DELAY);
+  }
 }
